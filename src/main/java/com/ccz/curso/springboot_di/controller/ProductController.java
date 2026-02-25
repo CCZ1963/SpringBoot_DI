d0f0c9be6c82e8ce82f0d525f33d548d598ad3a3
CECZ <cesarcantelli@gmail.com>
2026-02-25 19:48:29 -0300

    Creando la clase Controller - Video 48

---
+++ src/main/java/com/ccz/curso/springboot_di/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.ccz.curso.springboot_di.controller;
+
+import com.ccz.curso.springboot_di.model.Product;
+import com.ccz.curso.springboot_di.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    private ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id){
+        return productService.findById(id);
+    }
+}
